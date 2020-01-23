fc73a8175a36a06800c6394e7a314d0510b8e26c
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-01-22 20:13:24 -0500

    menu for tickets

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,12 +15,12 @@ concert2 = Concert.create(artist_name: "Drake", venue_name: "Webster Hall", conc
 concert3 = Concert.create(artist_name: "Imagine Dragons", venue_name: "Brooklyn Mirage", concert_date: "3/7/2020", price: 65)
 concert4 = Concert.create(artist_name: "Arctic Monkeys", venue_name: "Ballroom", concert_date: "6/5/2020", price: 400)
 concert5 = Concert.create(artist_name: "Wyclef Jean", venue_name: "Madison Square Garden", concert_date: "8/19/2020", price: 600)
-concert6 = Concert.create(artist_name: "Billy Joel", venue_name: "Barclays Center", concert_date: "4/17/2020", price: 185)
-concert7 = Concert.create(artist_name: "Elton John", venue_name: "Terminal 5", concert_date: "5/6/2020", price: 95)
-concert8 = Concert.create(artist_name: "ACDC", venue_name: "Madison Square Garden", concert_date: "10/1/2020", price: 300)
-concert9 = Concert.create(artist_name: "Ariana Grande", venue_name: "City Field", concert_date: "6/10/2020", price: 255)
-concert10 = Concert.create(artist_name: "Khalid", venue_name: "Barclays Center", concert_date: "6/19/2020", price: 350)
-concert11 = Concert.create(artist_name: "Lady Gaga", venue_name: "Kings Theater", concert_date: "12/21/2020", price: 150)
+# concert6 = Concert.create(artist_name: "Billy Joel", venue_name: "Barclays Center", concert_date: "4/17/2020", price: 185)
+# concert7 = Concert.create(artist_name: "Elton John", venue_name: "Terminal 5", concert_date: "5/6/2020", price: 95)
+# concert8 = Concert.create(artist_name: "ACDC", venue_name: "Madison Square Garden", concert_date: "10/1/2020", price: 300)
+# concert9 = Concert.create(artist_name: "Ariana Grande", venue_name: "City Field", concert_date: "6/10/2020", price: 255)
+# concert10 = Concert.create(artist_name: "Khalid", venue_name: "Barclays Center", concert_date: "6/19/2020", price: 350)
+# concert11 = Concert.create(artist_name: "Lady Gaga", venue_name: "Kings Theater", concert_date: "12/21/2020", price: 150)
 
 
 # ticket1 = Ticket.create(user_id: user1.id, concert_id: concert2.id)
--- lib/app/concert_app.rb
+++ lib/app/concert_app.rb
@@ -4,10 +4,10 @@ class ConcertApp
     # require 'tty-prompt'
 
     def run
-        # puts "Running!"
         name = welcome
         login_choices(name)
-        # signin
+        buy_edit_cancel
+        list_concerts
     end
 
     def welcome
@@ -16,6 +16,10 @@ class ConcertApp
         name = $prompt.ask("What's your name?")
         name
     end 
+
+    # # def find_user(string)
+    # #     User.all.find_by(name: string)
+    # end 
 
     def login_choices(name)
         choices = $prompt.select ("Hi #{name}! What would you like to do?") do |menu|
@@ -34,8 +38,63 @@ class ConcertApp
     def signup
         new_username = $prompt.ask("What would you like your username to be?")
         new_password = $prompt.mask("What would you like your password to be?")
-        new_user = User.create(username: new_username, password: new_password) 
+        new_user = User.create(username: new_username, password: new_password)     
     end
+
+    def buy_edit_cancel
+        option = $prompt.select("What would you like to do?") do |menu|
+            menu.choice "Buy Ticket", 1
+            menu.choice "Edit Ticket", 2
+            menu.choice "Cancel Ticket", 3
+        end
+
+        option
+    end 
+
+    def optional
+        case 
+            when buy_edit_cancel == 1  # if customer picks first choice (Buy tickt)
+                list_concerts
+                #
+                #new_ticket = Ticket.create()
+
+
+        end 
+    end 
+
+
+    def list_concerts
+        list = [] 
+        Concert.all.each do |concert|
+            list << concert 
+        end 
+            
+        concert_input = $prompt.select ('These are the concerts of 2020') do |menu|
+            menu.choice list[0].artist_name , 1
+            menu.choice list[1].artist_name , 2
+            menu.choice list[2].artist_name , 3
+            menu.choice list[3].artist_name , 4
+            menu.choice list[4].artist_name , 5
+        end  
+
+        case 
+            when concert_input == 1
+                new_ticket = Ticket.create(@user.id, @concert.id)  #ticket1 = Ticket.create(user_id: user1.id, concert_id: concert2.id)
+
+            when concert_input == 2
+                new_ticket = Ticket.create(@user.id, @concert.id)  
+
+            when concert_input == 3
+                new_ticket = Ticket.create(@user.id, @concert.id)  
+
+            when concert_input == 4
+                new_ticket = Ticket.create(@user.id, @concert.id)  
+
+            when concert_input == 5
+                new_ticket = Ticket.create(@user.id, @concert.id)  
+
+        end 
+    end 
 
     #     if login_choices == "Sign In"
     #         signin
@@ -69,3 +128,6 @@ class ConcertApp
 #     puts "Okay, #{@user.username.capitalize}!"
 # end 
 end
+
+# run 
+
